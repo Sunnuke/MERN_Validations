c5661aff7e73d3f00a230340b3ee695e53554b07
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-20 13:00:36 -0800

    Setup author.model.js with a schema: AuthorSchema

---
+++ server/models/author.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const AuthorSchema = new mongoose.Schema({
+    // Strings
+    name: {type: String, 
+        required: [true, "Author's Name is Required!"], 
+        minlength: [3, "The Author's Name must be at least 3 characters long!"]
+    },
+}, {timestamps: true});
+
+module.exports.Author = mongoose.model("Author", AuthorSchema);
