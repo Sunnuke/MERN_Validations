4d5438f591820dd95b127f4df1d895e1018d73c8
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-20 15:49:36 -0800

    Setup App.js and imported views

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Router } from "@reach/router";
 import './App.css';
+import AuthorList from "./views/AuthorList";
+import AuthorForm from "./views/AuthorForm";
+import AuthorEdit from "./views/AuthorEdit";
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
-        <Authors path="" />
-        <AuthorForm path="/new" />
-        <AuthorEdit path="/edit/:id" />
-      </Router> */}
+      <Router>
+        <AuthorList path="" />
+        <AuthorForm path="new" />
+        <AuthorEdit path="edit/:id" />
+      </Router>
     </div>
   );
 }
